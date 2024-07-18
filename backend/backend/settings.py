3f8a4add484508cf7e0823038688aab523f39aeb
Samuel <samuelou510@gmail.com>
2024-07-17 20:12:27 -0400

    allowed hosts added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.58.75.114', 'http://react-django.s3-website.us-east-2.amazonaws.com/']
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://react-django.s3-website.us-east-2.amazonaws.com/",
