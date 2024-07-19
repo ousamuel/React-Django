1440b82ef72c925ac312282c514d4d828e307bb6
Samuel <samuelou510@gmail.com>
2024-07-19 15:59:15 -0400

    edited both url.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,20 +21,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-
-router = routers.DefaultRouter()
-router.register(r'projects', views.ProjectView, 'project')
-router.register(r'users', views.UserView, 'user')
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myProject.urls')),   
-    # path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
-
-    
-from rest_framework.routers import DefaultRouter
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/myProject/urls.py
+++ backend/myProject/urls.py
@@ -1,5 +1,3 @@
-# backend/myProject/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserView, ProjectView
@@ -9,6 +7,5 @@ router.register(r'users', UserView)
 router.register(r'projects', ProjectView)
 
 urlpatterns = [
-    path('users/', UserView.as_view({'get': 'list', 'post': 'create'})),
-    path('projects/', ProjectView.as_view({'get': 'list', 'post': 'create', 'delete':'destroy'})),
+    path('', include(router.urls)),
 ]
