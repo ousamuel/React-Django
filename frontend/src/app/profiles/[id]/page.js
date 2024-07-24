1e06e9996053d96c5014606fe9df5a83fb12b5e1
Samuel <samuelou510@gmail.com>
2024-07-24 17:38:20 -0400

    attempt with actual api addresss to check if env is wrong

--- frontend/src/app/profiles/[id]/page.js
+++ frontend/src/app/profiles/[id]/page.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export async function generateStaticParams() {
   const DB_HOST = process.env.NEXT_PUBLIC_DB_127;
   try {
-    const profiles = await axios.get(`${DB_HOST}/profiles/`);
+    // const profiles = await axios.get(`${DB_HOST}/profiles/`);
+    const profiles = await axios.get(`http://ec2-52-15-203-120.us-east-2.compute.amazonaws.com/api/profiles/`);
     return profiles.data.map((profile) => ({
       id: profile.id.toString(), // Ensure the ID is a string
     }));
