91a8f410ff3a3183fc7e42d77b45f4fa35e9dfd6
Samuel <samuelou510@gmail.com>
2024-07-24 14:07:33 -0400

    force s3 bucket add

--- frontend/src/app/projects/page.js
+++ frontend/src/app/projects/page.js
@@ -2,7 +2,7 @@
 
 const ProjectsLanding = () => {
   // Separate useForm hooks for each form
-  return <main className="text-center">Projects page</main>;
+  return <main className="text-center">Projects page landing</main>;
 };
 
 export default ProjectsLanding;
