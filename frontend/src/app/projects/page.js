ae3d0f0deb297d896ebaa633d8b4aba6a56fcd04
Samuel <samuelou510@gmail.com>
2024-07-24 13:34:32 -0400

    wrong import on projectspage

--- frontend/src/app/projects/page.js
+++ frontend/src/app/projects/page.js
@@ -1,20 +1,4 @@
 "use client";
-import {
-  Image,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownSection,
-  DropdownItem,
-  Button,
-} from "@nextui-org/react";
-import ProjectList from "@/components/ProjectList";
-import AllUsers from "@/components/AllUsers";
-import NavBar from "@/components/Navbar";
-import axios from "axios";
-import { set, useForm } from "react-hook-form";
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "./AuthContext";
 
 const ProjectsLanding = () => {
   // Separate useForm hooks for each form
