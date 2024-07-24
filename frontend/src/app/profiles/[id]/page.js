4c06da3e5269dab930d7e5fbda4b6ad62e6cf4b7
Samuel <samuelou510@gmail.com>
2024-07-24 17:22:03 -0400

    revert

--- frontend/src/app/profiles/[id]/page.js
+++ frontend/src/app/profiles/[id]/page.js
@@ -1,8 +1,6 @@
 import ClientProfilePage from "@/components/ClientProfilePage";
 import axios from "axios";
 export async function generateStaticParams() {
-  return [{ id: 1 }];
-
   const DB_HOST = process.env.NEXT_PUBLIC_DB_127;
   try {
     const res = await axios.get(`${DB_HOST}/profiles/`);
