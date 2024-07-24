9ab5102e246b8d30c1f8a467e5171aad29ee0699
Samuel <samuelou510@gmail.com>
2024-07-24 16:15:22 -0400

    trying try catch with axios and added npm export to yml file

--- frontend/src/app/profiles/[id]/page.js
+++ frontend/src/app/profiles/[id]/page.js
@@ -1,15 +1,18 @@
 import ClientProfilePage from "@/components/ClientProfilePage";
-
+import axios from "axios";
 export async function generateStaticParams() {
   const DB_HOST = process.env.NEXT_PUBLIC_DB;
-  const profiles = await fetch(`${DB_HOST}/profiles/`).then((res) => res.json())
-
-  return profiles.map((profile) => ({
-    id: profile.id.toString(), // Ensure the ID is a string
-  }));
-
+  try {
+    const res = await axios.get(`${DB_HOST}/profiles/`);
+    const profiles = res.data;
+    return profiles.map((profile) => ({
+      id: profile.id.toString(), // Ensure the ID is a string
+    }));
+  } catch (error) {
+    console.error('Error fetching profiles:', error);
+    return [];
+  }
 }
-
 
 export default function ProfilePage({ params }) {
   const { id } = params;
