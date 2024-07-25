e1c74951807a0c2e2356113ce14ed892726f0067
Samuel <samuelou510@gmail.com>
2024-07-25 12:40:59 -0400

    removed / at url

--- backend/scripts.sh
+++ backend/scripts.sh
@@ -1,9 +1,11 @@
+cd /var/www/React-Django/backend
+git pull
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-# python manage.py flush --> clears db
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
+# python manage.py flush --> clears db
